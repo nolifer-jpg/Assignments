335eabb57dd490aea49e4d15af46e8a32ee1bab6
nolifer_jpg <roysouhard2706@gmail.com>
2025-05-18 14:36:11 +0530

    update

--- even_odd/even_odd.py
+++
@@ -1,5 +0,0 @@
-inp = int(input("Enter a number: "))
-if inp % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
--- multiples_of_3/multiples.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Enter up to how much do you want to print: "))
-for i in range(n):
-    print(3 * (i + 1))
