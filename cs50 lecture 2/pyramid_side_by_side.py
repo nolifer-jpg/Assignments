1b7a65f0b0d4f7ae1317553641ee7aa0e929e113
nolifer_jpg <roysouhard2706@gmail.com>
2025-05-21 12:29:18 +0530

    update

--- cs50 lecture 2/pyramid_side_by_side.py
+++ cs50 lecture 2/pyramid_side_by_side.py
@@ -1,8 +1,13 @@
-n = int(input("Enter the height of the pyramid: "))
+while True:
+    n = int(input("Height (1–8): "))
+    if 1 <= n <= 8:
+        break
+
 for i in range(1, n+1):
     print(" "*(n-i), end="")  #left spaces  
     print("#"*i, end="")      #left #'s
-    print("  ", end="")       #2 spaces
+    print("  ", end="")       #gap
     print("#"*i)              #right #'s
+    
 
 
